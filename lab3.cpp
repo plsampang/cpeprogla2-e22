9e06148dbe22ba289a9c9f65ea7a5f0ab788b248
PAOLO LUIS SAMPANG <PLSAMPANG@fit.edu.ph>
2016-09-10 11:20:34 +0800

    Initial lab3

---
+++ lab3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int myStrCmp(char* str1, char* str2){
+	return strcmp(str1, str2);
+}
+
+int main(){
+	char str1[100];
+	char str2[100];
+	int num;
+	cout<<"Enter a first word ";
+	cin>>str1[100];
+	cout<<"\nEnter a second word ";
+	cin>>str2[100];
+
+}
+
